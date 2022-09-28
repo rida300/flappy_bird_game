143f0abaf128dd1fe11a6ff36ea318de955b3dbe
Rida Ahmed <ahmed@civicplus.com>
2022-09-28 00:58:07 -0500

    build ai fitness function

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,5 +1,5 @@
 import pygame
-# import neat
+import neat
 import time
 import os
 import random
@@ -18,8 +18,7 @@ BASE_IMG = pygame.transform.scale2x(
 BG_IMG = pygame.transform.scale2x(
     pygame.image.load(os.path.join("imgs", "bg.png")))
 
-STAT_FONT = pygame.font.SysFont("comicsans",50)
-
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 
 class Bird:
@@ -60,17 +59,17 @@ class Bird:
         # 2nd second: (-10.5 * 2) +(1.5 * 2^2) = -21 + (1.5 * 4) = -21 + 6 = -15 IDK
 
         # for keeping bird in the frame, only allow it to move 16 pixels down max
-        if d >= 16:
-            d = 16
+        if displacement >= 16:
+            displacement = 16
 
         # makes jump higher, random number
-        if d < 0:
-            d -= 2
+        if displacement < 0:
+            displacement -= 2
 
-        self.y = self.y + d
+        self.y = self.y + displacement
 
         # if bird is moving upwards (d<0) or there's still space to go up then tilt bird upwards
-        if d < 0 or self.y < self.height + 50:
+        if displacement < 0 or self.y < self.height + 50:
             if self.tilt < self.MAX_ROTATION:
                 self.tilt = self.MAX_ROTATION
         else:  # tilt birds downwards
@@ -203,7 +202,7 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(window, bird, pipes, base, score):
+def draw_window(window, birds, pipes, base, score):
     # draw background image, blit is a method that draws
     # seond parameter is where you want to draw the image (top left position for the background image)
     window.blit(BG_IMG, (0, 0))
@@ -212,76 +211,167 @@ def draw_window(window, bird, pipes, base, score):
     for pipe in pipes:
         pipe.draw(window)
 
-    text = STAT_FONT.render("Score: "+str(score),1,(255,255,255))
-    window.blit(text, (WINDOW_WIDTH - 10 - text.get_width(),10))
+    text = STAT_FONT.render("Score: "+str(score), 1, (255, 255, 255))
+    window.blit(text, (WINDOW_WIDTH - 10 - text.get_width(), 10))
 
     base.draw(window)
 
-    bird.draw(window)
+    for bird in birds:
+        bird.draw(window)
     pygame.display.update()  # refresh the display
 
 
-def main():
+# every fitness function requires these 2 parameters, genomes and config
+def main(genomes, config):
+    # we're going to run it so all the birds are playing the same game
 
-    score = 0
-    # create bird with starting position (200,200)
-    birdd = Bird(230, 350)
+    # the following 2 lists are needed to keep track of the output of generations
+    nets = []
+
+    # ge is keeping track of all genomes, it is a list of tupes (genome id, genome object) id is int
+    ge = []
+    birdds = []  # Bird(230, 350)
+
+    for _, g in genomes:
+        # set up a neural network for this genome
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        # create bird with starting position (200,350)
+        birdds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
     base = Base(730)
 
     pipes = [Pipe(600)]
     # this creates one single pipe which will move and make its way out of the frame eventually
     # need to keep drawing pipes as long asgame is running
 
+    score = 0
+    clock = pygame.time.Clock()
+
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
     # setting up main loop to keep the game running
     run = True
     while run:
+        clock.tick(30)
         for event in pygame.event.get():  # loops through user events (e.g.: mouse click)
             if event.type == pygame.QUIT:  # clicking red X in the top right corner
                 run = False
+                pygame.quit()
+                quit()
 
 
 # make a list to add removed/ off screen pipes to
         rem = []
 
+        # there could be 2 pipes on the creen at the same time as the distance between them is randomly generated
+        # apparenlt max of 2 pipes at all times
+        pipe_index = 0
+        if len(birdds) > 0:
+            if len(pipes) > 1 and birdds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                # birdds[0].x using any random bird's x position as they are all the same
+                # bird has passed the first pipe
+                pipe_index = 1
+        else:
+            # no birds left, quit the game
+            run = False
+            break
+
+        # move/jump birds
+        for x, bird in enumerate(birdds):
+            bird.move()
+            # this loop runs 30 times in 1 second, every second the bird stays alive, it gains 3 fitness points which encourages the bird to keep moving forward and not upwards or downwards
+            ge[x].fitness += 0.1
+
+            # activate neural network by passing all our inputs and record its output
+            # 1. Input 1: bird's y coordinate
+            # 3. Input 2: distance between bird's y and the top of the top pipe
+            # 2. Input 3: distance between bird's y and the bottom of the top pipe
+            output = nets[x].activate(
+                (bird.y, abs(bird.y - pipes[pipe_index].height), abs(bird.y - pipes[pipe_index].bottom)))
+
+            # using tanh function to calculate the output
+            # output is a list with an element for each generation
+            if output[0] > 0.5:
+                bird.jump()
+
         add_pipe = False
 
         # move pipes
         for pipe in pipes:
-            if pipe.collide(birdd):
-                pass
+            for x, bird in enumerate(birdds):
+                if pipe.collide(bird):
+                    # everytime a bird collides with a pipe, subtract from its fitness
+                    ge[x].fitness -= 1
+                    birdds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
 
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 # means pipe is off the screen / out of frame now, new pipe needs drawn
                 # remove this pipe
                 rem.append(pipe)
 
-            if not pipe.passed and pipe.x < birdd.x:
-                pipe.passed = True
-                add_pipe = True
-
             pipe.move()
 
         if add_pipe:
             score += 1
-            pipes.append(Pipe(600)) #600 is the x position from one pipe to another, if we want the pipes to be closer together, we can shorten this
+
+            # add to the birds' fitness as they've passed through a new pipe, no need to loop hrough the birds to check if they're alive before updating their genome because dead birds' genome is removed from the list ^
+            for g in ge:
+                g.fitness += 5
+
+            # 600 is the x position from one pipe to another, if we want the pipes to be closer together, we can shorten this
+            pipes.append(Pipe(600))
 
         for r in rem:
             pipes.remove(r)
-        
-        if birdd.y + birdd.img.get_height() >= 730:
-            #bird has hit the floor
-            pass
+
+        for x, bird in enumerate(birdds):
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+                # bird has hit the floor or it has jumped too high off the screen
+                birdds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
 
         # move the base
         base.move()
 
-        draw_window(window, birdd, pipes, base, score)
-
-    pygame.quit()
-    quit()
+        draw_window(window, birdds, pipes, base, score)
 
 
-# if __name__ == "__main__":
-main()
+def run(config_path):
+    # read in config values from the file
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    # generate a population based on what we had in the config file
+    p = neat.Population(config)
+
+    # get stats about generations as they play the game, this info will appear in the console output
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    # set up the fitness function for 50 generations (so the fitness function will run 50 times)
+    # 1. the way to determine a bird's fitness is to see how far it can make it into the game
+    # 2. Using main as our fitness function
+    winner = p.run(main, 50)
+
+
+if __name__ == "__main__":
+    # get path for current directory
+    local_dir = os.path.dirname(__file__)
+
+    # find absolute path to config file
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+
+    run(config_path)
+
+
+# main()
